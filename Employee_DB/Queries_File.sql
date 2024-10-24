0ca624bce7cad7ca76180ee2c5a42ce50d3b347f
Amith.G <amithraj434@gmail.com>
2024-10-24 16:24:27 +0530

    Added Queries

--- Employee_DB/Queries_File.sql
+++ Employee_DB/Queries_File.sql
@@ -16,3 +16,17 @@ from employees e
 join departments d on e.department_id = d.department_id
 group by d.department_id
 having Dept_Salary > 15000;
+
+-- For each employee, display there salary on the note if its above or below 7000
+select first_name, case when salary >= 7000 then 'above 7000' else 'below 7000' end as Salary_Scale
+from employees;
+
+-- List the Projects that started last 1 months
+select * from projects
+where date_sub(curdate(), interval 1 month) <= start_date;
+
+-- Display all Projects, including with those no employees
+select e.first_name, p.project_name
+from employees e
+join employeeprojects ep on e.employee_id = ep.employee_id
+right join projects p on p.project_id = ep.project_id;
